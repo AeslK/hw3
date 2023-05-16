8bf1072f9d0e858f2bcbff3e8078b62826930e29
Aesl Kim <aesl.kim@kellogg.northwestern.edu>
2023-05-16 00:37:46 +0000

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Place.destroy_all
+
+mexicocity = Place.new
+mexicocity["name"] = "Mexico City"
+mexicocity.save
+
+charleston = Place.new
+charleston["name"] = "Chaleston"
+charleston.save
+
+beijing = Place.new
+beijing["name"] = "Beijing"
+beijing.save
+
+amsterdam = Place.new
+amsterdam["name"] = "Amsterdam"
+amsterdam.save
+
